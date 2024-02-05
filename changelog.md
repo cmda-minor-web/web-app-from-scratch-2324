986e92dc60fdf9c43e21727075e4e449fab414cd
bazottie <bas@avocado-media.nl>
2024-02-05 21:19:45 +0100

    Added base styling to pages

--- changelog.md
+++ changelog.md
@@ -2,3 +2,4 @@
 ## Week 1
 ### 05/02/2024
 - Tabel met teams en beschrijving toegevoegd
+- Overzicht pagina basic styling gegeven
---
+++ docs/scripts/index.js
@@ -0,0 +1,34 @@
+main().then(() => console.log('done'))
+
+async function main() {
+    const students = await getStudents();
+    renderStudents(students.sort(() => 0.5 - Math.random()))
+}
+
+function renderStudents(students) {
+    const container = document.querySelector('[data-students]');
+    const list = document.createElement('ul')
+
+    students.map(student => {
+        const item = document.createElement('li')
+        const anchor = document.createElement('a');
+        const description = document.createElement('p')
+        const avatar = document.createElement('img')
+        anchor.href = `https://${student.login}.github.io/web-app-from-scratch-2324/`
+        anchor.alt = `WAFS fork from ${student.login}`
+        anchor.target = '_blank'
+        anchor.textContent = student.login
+        avatar.src = student.avatar_url
+        item.append(avatar)
+        item.append(anchor)
+        list.append(item)
+    })
+    container.append(list)
+}
+
+async function getStudents() {
+    const res = await fetch('https://api.github.com/repos/cmda-minor-web/web-app-from-scratch-2324/forks')
+    const teams = await res.json()
+    console.log(teams);
+    return teams.map(({owner}) => owner)
+}
