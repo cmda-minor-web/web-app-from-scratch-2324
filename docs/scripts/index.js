d397fe3e970893e2665019c3721e00eb3963d858
Manoah Tervoort <46671786+mtdvlpr@users.noreply.github.com>
2024-02-06 14:01:04 +0100

    fix: fetch up to 100 forks

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -27,7 +27,7 @@ function renderStudents(students) {
 }
 
 async function getStudents() {
-    const res = await fetch('https://api.github.com/repos/cmda-minor-web/web-app-from-scratch-2324/forks')
+    const res = await fetch('https://api.github.com/repos/cmda-minor-web/web-app-from-scratch-2324/forks?per_page=100')
     const teams = await res.json()
     console.log(teams);
     return teams.map(({owner}) => owner)
